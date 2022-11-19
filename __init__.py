82498b6696cd9e1e86718b1d3e806bff866a6e97
Talha <mtalha7274@gmail.com>
2022-11-20 01:02:33 +0500

    login api created

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from flask_restful import Api
-from .endpoints import GenerateImages
+from .endpoints import b0
+from .env.secrets import SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app)
 
-    api.add_resource(GenerateImages, '/<string:prompt>')
+
+    app.config['SECRET_KEY'] = SECRET_KEY
+
+
+    app.register_blueprint(b0)
+
+
     return app
--- endpoints.py
+++ endpoints.py
@@ -1,7 +1,29 @@
-from flask_restful import Resource
+from flask import Blueprint, request, jsonify, current_app
 from .dalle_api_call import create_images
+from .env.secrets import APP_USERNAME, APP_PASSWORD
+import jwt
+from datetime import datetime, timedelta
 
 
-class GenerateImages(Resource):
-    def get(self, prompt):
-        return create_images(prompt)
+b0 = Blueprint('GenerateImages', __name__)
+
+
+@b0.route('/<string:prompt>')
+def getImages(prompt):
+    return create_images(prompt)
+
+
+@b0.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] == APP_USERNAME and request.form['password'] == APP_PASSWORD:
+        
+        token = jwt.encode({
+          'user': request.form['username'],
+          'expiration': str(datetime.utcnow() + timedelta(weeks=4))
+        },
+        current_app.config['SECRET_KEY']
+        )
+        return jsonify({'token': token.encode().decode('utf-8')})
+    return jsonify({'error': 'unable to verify'})
+
+  
