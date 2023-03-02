0d35063d4b895532667093def6c7a0dd8d06c5b3
Talha <mtalha7274@gmail.com>
2023-03-02 14:59:16 +0500

    some changes

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ app.register_blueprint(b0)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+# use this to activate virtual environment  .\env\Scripts\activate.ps1
+# use this to run server    python .\main.py
